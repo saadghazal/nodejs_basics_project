85d78162582f677c4fe4f259c03199404839bd06
saadghazal <saad.m.ghazal@icloud.com>
2023-12-08 19:47:01 +0300

    target different respnse based on a different requests

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const http = require('http')
 
 // it requires to accept requestListener and it's a function that will execute for every incoming request
 const server = http.createServer((req,res)=>{
+    let url = req.url
+    if(url === '/'){
+        res.setHeader('Content-Type','text/html')
+        res.write("<html>")
+        res.write('<head><title>My First Page</title></head>')
+        res.write(`<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`)
+        res.write("</html>")
+        return res.end()
+    }
     let name = "Ghazal"
     res.setHeader('Content-Type','text/html')
     res.write("<html>")
