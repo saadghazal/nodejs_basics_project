7fc49c4ab53f94aa74667a6bab4195e5c938367c
saadghazal <saad.m.ghazal@icloud.com>
2023-12-08 19:39:03 +0300

    sending html page a response to the incoming request

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const http = require('http')
 
 // it requires to accept requestListener and it's a function that will execute for every incoming request
 const server = http.createServer((req,res)=>{
-    console.log(req)
+    let name = "Ghazal"
+    res.setHeader('Content-Type','text/html')
+    res.write("<html>")
+    res.write('<head><title>My First Page</title></head>')
+    res.write(`<body><h1>Hello From Node Js with ${name}</h1></body>`)
+    res.write("</html>")
+    res.end()
 })
 
 server.listen(3000)
