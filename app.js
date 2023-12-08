4e1aa5388e5734f64077f977c6b43168e8bae9fa
saadghazal <saad.m.ghazal@icloud.com>
2023-12-08 20:02:55 +0300

    redirection

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http')
+const fs = require("fs")
 
 // it requires to accept requestListener and it's a function that will execute for every incoming request
 const server = http.createServer((req,res)=>{
     let url = req.url
+    let method = req.method
     if(url === '/'){
         res.setHeader('Content-Type','text/html')
         res.write("<html>")
@@ -10,6 +12,12 @@ const server = http.createServer((req,res)=>{
         res.write(`<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`)
         res.write("</html>")
         return res.end()
+    }
+    if(url === "/message" && method === "POST"){
+        fs.writeFileSync("message.txt",'DUMMY')
+        res.statusCode = 302 //redirection
+        res.setHeader('Location','/')
+        return res.end();
     }
     let name = "Ghazal"
     res.setHeader('Content-Type','text/html')
