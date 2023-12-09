1a770c4f8711614daf1731d2aee0403e96681f7c
saadghazal <saad.m.ghazal@icloud.com>
2023-12-09 17:22:36 +0300

    Streams & Buffers Concept

--- app.js
+++ app.js
@@ -13,8 +13,29 @@ const server = http.createServer((req,res)=>{
         res.write("</html>")
         return res.end()
     }
-    if(url === "/message" && method === "POST"){
-        fs.writeFileSync("message.txt",'DUMMY')
+    if(url === "/message" && method === "POST"){ 
+        const body = [];
+        // getting user data in chunks with buffers concept.
+        req.on('data',(chunk)=>{
+            // Note --> We Can't work with the chunk itself
+            console.log("Chunk:",chunk)
+            body.push(chunk)
+        }); // on method allows us to listen to certain events and here i'm listening to the data event.
+        /* 
+        ****** Data Event: ******
+        The data event will be fired whenever a new chunk is ready to be read.
+        */
+       req.on('end',()=>{
+        // in this function we can rely on all the chunks being read in.
+        const parsedBody = Buffer.concat(body).toString()
+        const message = parsedBody.split('=')[1]
+        fs.writeFileSync("message.txt",message)
+       })
+       /*
+        ****** End Event: ******
+        This end event will be fired once it's done parsing the incoming requests data
+        or the incoming requests in general.
+       */
         res.statusCode = 302 //redirection
         res.setHeader('Location','/')
         return res.end();
